19aabd1011789410c12db10109d2262476b532e5
Ryan Hanrahan <rhanrahan99@gmail.com>
2026-03-01 17:54:10 -0500

    created registration page file and made additional adjustments to login page

---
+++ allies-connect/src/pages/registration.js
@@ -0,0 +1,12 @@
+import { Container, Tabs, Tab, Form, Button, Row, Col } from "react-bootstrap";
+import '../App.css';
+
+function Registration() {
+    return (
+        <Container className="registration-container">
+            
+        </Container>
+    );
+}
+
+export default Registration;
