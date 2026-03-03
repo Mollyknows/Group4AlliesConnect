c65178639853b9af28b155dfc3fcca1c4d554861
Ryan Hanrahan <rhanrahan99@gmail.com>
2026-03-03 18:59:53 -0500

    modified app.js to resolve merge conflict

--- allies-connect/src/App.js
+++ allies-connect/src/App.js
@@ -1,20 +1,28 @@
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import MyNavbar from './components/navbar';
+import Events from "./pages/events";
 import Home from './pages/home';
 import Login from './pages/login';
 import Register from './pages/register';
+import Maps from "./pages/maps";
 
 function App() {
   return (
-    <BrowserRouter>
-      <MyNavbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <BrowserRouter>
+        <MyNavbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/maps" element={<Maps />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </LocalizationProvider>
   );
 }
 
